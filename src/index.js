eb0400d57979f6e06e96dabd739d64d253ef9956
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-08-24 10:24:26 -0400

    Initialization + npm script edits + dropdown setup
    Creates buttons for dropdown setup.
    
    Current bug: Prettier-setup script not working entirely.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./styles.css";
 
-console.log("Testing");
+
+// NOTES & PSEUDOCODE
+// Create a node array of all items with the class of dropdown-menu
+    // Only created IF there are items with that class created
+// All items marked with dropdown-item will default to hidden
+// On click of dropdown-menu its div child with dropdown-item will show
+    // Shown as absolute to the relative of the button
+// Event listener on item div mouseout will set style to hidden again
+    // Make item div wide enough to not make it annoying
+// Must be repeatable with several different "dropdown-menus"
+    // Publish the module to npm for quicker use
