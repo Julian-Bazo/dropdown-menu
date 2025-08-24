b14e8e1218f72f1ce20b48e65a2ed4eab7ea1251
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-08-24 18:37:30 -0400

    Clean code + add tutorial to the bottom of module
    Cleans up unnecessary junk code and adds a tutorial for proper class naming.

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,8 +1,6 @@
 export default function createDropdown() {
 let dropDownArray  = document.querySelectorAll(".dropdown");
 dropDownArray = Array.from(dropDownArray);
-console.log(dropDownArray);
-
 
 dropDownArray.map((dropdown) => {
     const dropDownButton = dropdown.querySelector(".dropdown-button");
@@ -27,30 +25,17 @@ dropDownArray.map((dropdown) => {
     })
 
 })
+})};
 
 
-})
-// const dropDownButton = document.querySelector(".dropdown-button");
-// const dropDownWindow = document.querySelector(".dropdown-items");
+// PROPER CLASS SETUP
 
-// dropDownArray.style.position = "relative";
-// dropDownWindow.style.display = "none";
-
-// dropDownWindow.style.paddingTop = "2rem";
-// dropDownWindow.style.paddingBottom = "2rem";
-// dropDownButton.addEventListener("mouseenter", () => {
-//     dropDownWindow.style.display = "flex";
-//     dropDownWindow.style.flexDirection = "column";
-//     dropDownWindow.style.gap = "1rem";
-//     dropDownWindow.style.position = "absolute";
-// })
-
-// dropDownWindow.addEventListener("mouseleave", () => {
-//     dropDownWindow.style.display = "none";
-// })
-}
-
-// Change selectors to child selectors .dropdown can stay, just
-// make an Array of it
-
-// Test by creating others
+//    <div class="container">
+//         <div class="dropdown">
+//             <button class="dropdown-button">Hover me!</button>
+//                 <div class="dropdown-items">
+//                     <button>Edit</button>
+//                     <button>Copy</button>
+//                     <button>Delete</button>
+//                 </div>
+//         </div>
